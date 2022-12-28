3bab4681c9bcf42d8b82eaa7273908abee88f954
ahsfar <ahsfar21@gmail.com>
2022-12-28 12:59:41 +0800

    navbar completed with css

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,10 +1,24 @@
 
+import Logo from "../../assets/logo.jpg";
+import data from "./data";
+import { AiOutlineBgColors} from "react-icons/ai"
 import "./navbar.css"
 
 function Navbar() {
   return (
     <nav>
-        <h2>Navbar</h2>
+    {/* nav__container -> BEM - block element modifier class */}
+        <div className="container nav__container">
+          <a href="index.html" className="nav__logo">
+            <img src={Logo} alt="Logo" />
+          </a>
+          <ul className="nav__menu" >
+            {
+              data.map(item=> <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+            }
+          </ul>
+          <button id="theme__icon"><AiOutlineBgColors/></button>
+        </div>
     </nav>
   )
 }
