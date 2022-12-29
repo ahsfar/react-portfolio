573d97e96f999d3b8dfc5d2760aa8217d0fe1656
ahsfar <ahsfar21@gmail.com>
2022-12-29 12:02:07 +0800

    header section with CSS @media

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import HeaderImage from "../../assets/header.jpg"
+import data from "./data"
 import "./header.css"
 
 function Header() {
   return (
     <header id="header">
-        <h2>Header</h2>
+        <div className="container header__container">
+          <div className="header__profile">
+            <img src={HeaderImage} alt="Header Portrait" />
+          </div>
+          <h3>Hajia Bintu</h3>
+          <p>
+            You are a click away from building your dream website or web app. Send me the details o fyour project for a modern, mobile responsive, highly performant website today!
+          </p>
+          <div className="header__cta">
+            <a href="#contact" className="btn primary">Let's talk</a>
+            <a href="#portfolio" className="btn light">My Work</a>
+          </div>
+          <div className="header__socials">
+            {
+              data.map(item =><a key={item.id} href={item.link} target="_blank"
+              rel="noopener noreferrer">{item.icon}</a>)
+            }
+          </div>
+        </div>
     </header>
   )
 }
---
+++ src/sections/header/data.js
@@ -0,0 +1,14 @@
+import {AiOutlineInstagram} from "react-icons/ai";
+import {AiOutlineTwitter} from "react-icons/ai";
+import {AiOutlineDribbble} from "react-icons/ai";
+import {AiFillGithub} from "react-icons/ai";
+
+const data = [
+    {id:1, link: "http://instagram.com", icon: <AiOutlineInstagram/>},
+    {id:1, link: "http://twitter.com", icon: <AiOutlineTwitter/>},
+    {id:1, link: "http://dribble.com", icon: <AiOutlineDribbble/>},
+    {id:1, link: "http://github.com", icon: <AiFillGithub/>}
+
+]
+
+export default data
