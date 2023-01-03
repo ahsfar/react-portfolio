20c942b21f9e22662fda9ab1d1eab40e6c56c13f
ahsfar <ahsfar21@gmail.com>
2023-01-03 12:01:04 +0800

    faqs sec with css - faqs toggle using usState hook

--- src/sections/faqs/FAQs.jsx
+++ src/sections/faqs/FAQs.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./faqs.css"
+import faqs from "./data"
+import FAQ from "./FAQ"
 
 function FAQs() {
   return (
     <section id='faqs'>
-      <h2>FAQs</h2>
+      <h2>Frequently Asked Questions</h2>
+      <p>
+        Here are some of the questions I usually get. Click to toggle the answer, and if you still have some more questions, shoot me a message from the contact section.
+      </p>
+        <div className="container faqs__container">
+          {
+            faqs.map(faq=>(
+              <FAQ key={faq.id} faq={faq}/>
+            ))
+          }
+        </div>
     </section>
   )
 }
