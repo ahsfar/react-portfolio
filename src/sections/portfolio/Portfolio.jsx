8b37261f53cb1994aaa940460970f6c31a6f4966
ahsfar <ahsfar21@gmail.com>
2023-01-03 09:09:24 +0800

    Projects with CSS - useState for filtering project

--- src/sections/portfolio/Portfolio.jsx
+++ src/sections/portfolio/Portfolio.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import ProjectsCategories from "./ProjectsCategories"
+import Projects from "./Projects"
 import "./portfolio.css"
+import data from "./data"
+import {useState} from "react"
+
 
 function Portfolio() {
+
+  const [projects, setProjects] = useState(data);
+
+  const categories = data.map(item => item.category)
+  const uniqueCategories = ["all", ... new Set(categories)];
+
+  const filterProjectsHandler = (category) => {
+    if (category === "all"){
+      setProjects(data);
+      return;
+    }
+    const filterProjects = data.filter(project => project.category === category)
+    setProjects(filterProjects)
+  }
+
   return (
     <section id="portfolio">
-        <h2>Portfolio</h2>
+        <h2>Recent Projects</h2>
+        <p>
+          Check out some of the projects I recently worked on for my clients. Use the buttons to toggle the different catgories.
+        </p>
+        <div className="container portfolio__container">
+          <ProjectsCategories categories={uniqueCategories} onFilterProjects={filterProjectsHandler}/>
+          <Projects projects={projects}/>
+        </div>
     </section>
   )
 }
