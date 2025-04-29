eee57cb6f4a431b6173a84d15f9b782491e90e5a
ahsfar <ahsfar21@gmail.com>
2025-04-30 05:28:54 +0800

    commented FAQ and Testimonials for now

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Header from "./sections/header/Header";
 import About from "./sections/about/About";
 import Services from "./sections/services/Services";
 import Portfolio from "./sections/portfolio/Portfolio";
-import Testimonials from "./sections/testimonials/Testimonials";
-import FAQs from "./sections/faqs/FAQs";
+// import Testimonials from "./sections/testimonials/Testimonials";
+// import FAQs from "./sections/faqs/FAQs";
 import Contact from "./sections/contact/Contact";
 import Footer from "./sections/footer/Footer";
 import FloatingNav from "./sections/floating-nav/FloatingNav";
@@ -53,8 +53,8 @@ const App = () => {
         <About />
         <Services />
         <Portfolio />
-        <Testimonials />
-        <FAQs />
+        {/* <Testimonials /> */}
+        {/* <FAQs /> */}
         <Contact />
         <Footer />
         <Theme/>
--- src/sections/faqs/FAQs.jsx
+++ src/sections/faqs/FAQs.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import "./faqs.css"
-import faqs from "./data"
-import FAQ from "./FAQ"
+// import React from 'react'
+// import "./faqs.css"
+// import faqs from "./data"
+// import FAQ from "./FAQ"
 
-function FAQs() {
-  return (
-    <section id='faqs'>
-      <h2>Frequently Asked Questions</h2>
-      <p>
-        Here are some of the questions I usually get. Click to toggle the answer, and if you still have some more questions, shoot me a message from the contact section.
-      </p>
-        <div className="container faqs__container" data-aos="fade-in">
-          {
-            faqs.map(faq=>(
-              <FAQ key={faq.id} faq={faq}/>
-            ))
-          }
-        </div>
-    </section>
-  )
-}
+// function FAQs() {
+//   return (
+//     <section id='faqs'>
+//       <h2>Frequently Asked Questions</h2>
+//       <p>
+//         Here are some of the questions I usually get. Click to toggle the answer, and if you still have some more questions, shoot me a message from the contact section.
+//       </p>
+//         <div className="container faqs__container" data-aos="fade-in">
+//           {
+//             faqs.map(faq=>(
+//               <FAQ key={faq.id} faq={faq}/>
+//             ))
+//           }
+//         </div>
+//     </section>
+//   )
+// }
 
-export default FAQs
+// export default FAQs
